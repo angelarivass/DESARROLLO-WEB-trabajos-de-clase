83f0566c524fa1374353e08117176a8d0785325c
angelarivass <arivas@cetys.edu.mx>
2025-08-30 14:53:02 -0700

    actividad de recursividad

---
+++ ActividadesFuncionesDinamicas/MenuRecursividad.js
@@ -0,0 +1,36 @@
+const menu = {
+    nombre: 'Todas las Categorías',
+    subcategorias: [
+        {
+            nombre: 'Electrónica',
+            subcategorias: [
+                { nombre: 'Celulares', subcategorias: [] },
+                { nombre: 'Laptops', subcategorias: [
+                    { nombre: 'Gaming', subcategorias: [] },
+                    { nombre: 'Oficina', subcategorias: [] }
+                ]}
+            ]
+        },
+        {
+            nombre: 'Ropa',
+            subcategorias: [
+                { nombre: 'Dama', subcategorias: [] },
+                { nombre: 'Caballero', subcategorias: [] }
+            ]
+        },
+        {
+            nombre: 'Hogar',
+            subcategorias: [] // Esta categoría no tiene subcategorías
+        }
+    ]
+};
+
+
+function imprimirMenu(categoria, nivel){
+    console.log('-'.repeat(nivel * 2) + categoria.nombre);
+    for(const sub of categoria.subcategorias){
+        imprimirMenu(sub, nivel + 1);
+    }
+};
+
+imprimirMenu(menu, 0);
